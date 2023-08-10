d95c66b86826e55e8d41589a21c00835238010f2
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-08-10 13:48:27 -0300

    update jupyter port to 8888

--- docker/work/entrypoint.sh
+++ docker/work/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-jupyter lab --port=8081 --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.token='' --NotebookApp.password='' & \
+jupyter lab --port=8888 --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.allow_remote_access=True --NotebookApp.trust_xheaders=True & \
 nginx -g "daemon off;" -c /build/nginx.conf
